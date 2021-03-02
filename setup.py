7aafcd328d3cec293a9c6bd58bdf858d09d9aabf
0ctopirate <octo@pirate.rip>
2021-03-01 15:46:23 -1000

    PACKAGE: added scripts to setup

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/8o-COLLECTIVE/cobalt8",
+    scripts=["scripts/cobalt8"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
