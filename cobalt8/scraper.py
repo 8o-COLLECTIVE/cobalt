78ec4d6acbcbc7153dd7c626951dc99e033f970f
0ctopirate <octo@pirate.rip>
2021-03-01 14:58:45 -1000

    CORE: complete scraper restructure

--- cobalt8/scraper.py
+++ cobalt8/scraper.py
@@ -4,53 +4,70 @@ import aes
 
 # Create an anonymous discord client, which retrieves the content of the selected channel and then dies.
 
-print("Establishing Discord client, please hold...")
 
-client = discord.Client()
+def scrape():
+    print("Establishing Discord client, please hold...")
+    
+    client = discord.Client()
+    
+    client.run('ODAxMjE4ODI4MDA0MjI5MTIw.YAdfLg._IxT1qxs3AaFFig-7e6GwC50c9s')
+    
+    @client.event
+    async def on_ready():
+        channel = get_channel
 
-@client.event
-async def on_ready():
+        key = input("\nConversation key: ")
+
+        aesobj = aes.AES(bytearray.fromhex(key))
+
+        key_hash = hashlib.md5(bytearray.fromhex(key)).hexdigest()
+
+        print("\nLoading conversation...\n")
+
+        if channel is not None:
+            multi_decrypt(key_hash)
+
+        else:
+            print("\nFailed to find channel with provided ID")
+            toScrape = input("\nDo you want to try again? [Y or N] ")
+            if(toScrape.lower() == "y"):
+                await on_ready()
+        await client.close()
+
+
+def get_channel():
     to_scrape = input("\nInput channel ID to be scraped [or press enter for #cobalt]: ")
-    print()
     if len(to_scrape) == 0:
         to_scrape = 813872961656193045
 
     try:
         channel = client.get_channel(int(to_scrape))
     except:
-        print("It would appear that key is invalid.")
+        print("\nIt would appear that key is invalid.")
         pass
+        
+    return channel
+    
+    
+def decrypt_text(text):
+    format_message = lambda datetime, author, dmessage: "[{}] {}: {}".format(datetime, author, dmessage)
+    text = item.content
+    text = text[32:]
 
-    key = input("Conversation key: ")
+    iv = text[:32]
+    text = text[32:]
+    
+    dmessage = aesobj.decrypt_ctr(bytearray.fromhex(text), bytearray.fromhex(iv)).decode('ascii')
+    
+    messagef = format_message(str(item.created_at), item.author.name, dmessage)
 
-    aesobj = aes.AES(bytearray.fromhex(key))
+    return messagef
+    
+    
+async def multi_decrypt(key_hash):
+    messages = await channel.history().flatten()
+            
+    stack = [decrypt_text(item.content) for item in messages if item.content.startswith(key_hash)]
 
-    key_hash = hashlib.md5(bytearray.fromhex(key)).hexdigest()
-
-    print("\nLoading conversation...\n")
-
-    if channel is not None:
-
-        messages = await channel.history().flatten()
-        stack = []
-        for item in messages:
-            if item.content.startswith(key_hash):
-                text = item.content
-                text = text[32:]
-
-                iv = text[:32]
-                text = text[32:]
-
-                stack.append("[" + str(item.created_at) + "] " + item.author.name + f": {aesobj.decrypt_ctr(bytearray.fromhex(text), bytearray.fromhex(iv)).decode('ascii')}")
-
-        for x in range(len(stack)):
-            print(stack.pop())
-
-    else:
-        print("\nFailed to find channel with provided ID")
-        toScrape = input("\nDo you want to try again? [Y or N] ")
-        if(toScrape.lower() == "y"):
-            await on_ready()
-    await client.close()
-
-client.run('ODAxMjE4ODI4MDA0MjI5MTIw.YAdfLg.B9dRxE5X7UmyhCmhf_-vEz4pRCY')
+    for x in range(len(stack)):
+        print(stack.pop())
