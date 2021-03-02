75b5a72bdd5557b395144cf37bc8269737d14d6c
0ctopirate <octo@pirate.rip>
2021-03-02 08:10:32 -1000

    VERSION: migrating to 0.0.3 for bugfixes

--- cobalt8/__version__.py
+++ cobalt8/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "cobalt8"
 __description__ = "Offline and secure AES encryption with a Discord context in mind. "
 __url__ = "https://github.com/8o-COLLECTIVE/cobalt8"
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 __author__ = "8o-COLLECTIVE"
 __author_email__ = "8ocollective@birdlover.com"
 __license__ = "MIT"
